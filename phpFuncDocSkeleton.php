aae638e91e7720cf2dfb990cac1a5f47c6890f67
i5heu <i5heu@i5h.eu>
2018-08-10 13:57:11 +0200

    Update phpFuncDocSkeleton.php

--- phpFuncDocSkeleton.php
+++ phpFuncDocSkeleton.php
@@ -1,13 +1,12 @@
 /**
-* Summery
-*
-* Description
-*
-* @param Request $request
-*
-* @return Response
-*/
-
+ * Summery
+ *
+ * Description
+ *
+ * @param Request $request
+ *
+ * @return Response
+ */
 public function NAME(Request $request)
 {
     //TODO
